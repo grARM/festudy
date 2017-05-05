60c68277f66fbc7a07506b3a2605bbd11c740d47
junhe213310 <junhe213310@sohu-inc.com>
2017-05-05 15:09:35 +0800

    fix

--- feCommon/homework/gulpJslintDemo/hj/gulpfile.js
+++ feCommon/homework/gulpJslintDemo/hj/gulpfile.js
@@ -1,15 +1,33 @@
 var gulp = require('gulp');
 var jslint = require('gulp-jslint');
 var Files = require('./files.json');
+var path = require('path');
+var through = require('through2');
+var fs = require('fs');
 
 gulp.task('jslint', function () {
     return gulp.src(Files.src)
             .pipe(jslint())
+            .pipe(
+                // 将已经检查的文件名写入checkdFiles字段
+                through.obj(function(file,enc,cb){
+                    if(Files.checkedFiles.indexOf(file.path) < 0){
+                        Files.checkedFiles[Files.checkedFiles.length] = file.path;
+                    }
+                    fs.writeFile('files.json', JSON.stringify(Files), function (err) {
+                        if (err) throw err;
+                        console.log('写入完成');
+                    });
+                    this.push(file);
+                    cb();
+                })
+            )
             .pipe(jslint.reporter( 'default' ));
 });
-gulp.task('default',['jslint']); 
 
 var watcher = gulp.watch(Files.src, ['jslint']);
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
+
+gulp.task('default',['jslint']); 
