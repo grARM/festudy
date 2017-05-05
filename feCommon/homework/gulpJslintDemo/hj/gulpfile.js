41c5133bb8154acadbd5ef2f24df0a49cce7f6bc
junhe213310 <junhe213310@sohu-inc.com>
2017-05-05 13:38:30 +0800

    fix

--- feCommon/homework/gulpJslintDemo/hj/gulpfile.js
+++ feCommon/homework/gulpJslintDemo/hj/gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var jslint = require('gulp-jslint');
- 
-gulp.task('default', function () {
-    return gulp.src(['huxing.js'])
+var Files = require('./files.json');
+
+gulp.task('jslint', function () {
+    return gulp.src(Files.src)
             .pipe(jslint())
             .pipe(jslint.reporter( 'default' ));
+});
+gulp.task('default',['jslint']); 
+
+var watcher = gulp.watch(Files.src, ['jslint']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
